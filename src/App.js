5f5b5fee0322f0cf08fd7bbddc7991fa3d194062
randradev <randradevald@gmail.com>
2023-10-26 19:56:42 -0300

    Estructura de prueba

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Pruebas: Prueba lo que quieras, pero esto tiene que quedar vacío, lo que insertes para ver el renderizado, después tienes que eliminarlo */}
+
     </div>
   );
 }
