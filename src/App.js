a6cfa6a6d05f76239c32a8aae1b440ab08d8cb9b
randradev <randradevald@gmail.com>
2023-10-27 13:55:59 -0300

    Agregados CDN Bootstrap CSS y JS + Montserrat font family

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       {/* Pruebas: Prueba lo que quieras, pero esto tiene que quedar vacío, lo que insertes para ver el renderizado, después tienes que eliminarlo */}
-
+      
     </div>
   );
 }
