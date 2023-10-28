920c7fe191ec88ea34572a07021c98df0cb5afb0
JersonAmir <147181633+JersonAmir@users.noreply.github.com>
2023-10-28 15:34:33 -0300

    Agrego Buscador

---
+++ src/components/Buscador/Buscador.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Buscador() {
+    return (
+    <div className='container-fluid'> 
+        <h1 className='buscador-h1'>Buscador</h1>
+        <p className='buscador-p'>Busca la campaña en la que quieras aportar.</p>
+        <form>
+            <input className='barra-busqueda' type="search" placeholder="Escribe tu búsqueda..." aria-label="Search"></input>
+        </form>
+    </div>
+    );
+}
+export default Buscador;
