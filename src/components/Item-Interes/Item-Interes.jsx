e183aecfdc1257731dc5db432c593d10b2e4adea
JersonAmir <147181633+JersonAmir@users.noreply.github.com>
2023-10-29 18:18:10 -0300

    Nuevos componentes agregados

---
+++ src/components/Item-Interes/Item-Interes.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Item-Interes.css'
+
+function Interes() {
+    return(
+        <div class="button">
+        <button class="button1">            
+            Vivienda
+        </button>
+
+        <button class="button2">
+            Salud            
+        </button>
+
+        <button class="button3">
+            Educación            
+        </button>
+
+        <button class="button4">
+            Infancia           
+        </button>
+
+        <button class="button5">
+            Rural            
+        </button>
+
+    </div>        
+    )
+}
+export default Interes;
