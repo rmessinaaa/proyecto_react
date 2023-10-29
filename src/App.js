6ffcb4c7a32b5bc8605854bd16215c96d1a04847
Renato Messina <138999302+rmessinaaa@users.noreply.github.com>
2023-10-28 21:37:13 -0300

    filtro e infopersonal (componentes)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
+import infoPersonal from 
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       {/* Pruebas: Prueba lo que quieras, pero esto tiene que quedar vacío, lo que insertes para ver el renderizado, después tienes que eliminarlo */}
       
     </div>
+    
   );
 }
 
--- src/components/Filtro/Filtro.jsx
+++ src/components/Filtro/Filtro.jsx
@@ -1,1 +1,74 @@
-  
+import React from "react";
+import Select from "react-select";
+import './Filtro.css'
+
+const categorias = [
+    { label : "Opción 1", value: "Opcion 1"},
+    { label : "Opción 2", value: "Opcion 2"},
+    { label : "Opción 3", value: "Opcion 3"}
+]
+const valores = [
+    { label : "Opción 1", value: "Opcion 1"},
+    { label : "Opción 2", value: "Opcion 2"},
+    { label : "Opción 3", value: "Opcion 3"}
+]
+const instituciones = [
+    { label : "Opción 1", value: "Opcion 1"},
+    { label : "Opción 2", value: "Opcion 2"},
+    { label : "Opción 3", value: "Opcion 3"}
+]
+function Filtro(){
+
+const handSelectCategoria = (e) => {
+            console.log(e);
+        }
+    
+const handSelectValor = (e) => {
+        console.log(e);
+    }
+
+const handSelectInstitucion = (e) => {
+        console.log(e);
+    }
+
+    return (
+    <div className="container">
+        <h3 className="titulo">Filtra tu búsqueda:</h3>
+            <h4 className="subtitulo">Categoría</h4>
+            
+            <Select 
+            className="opciones" 
+            options={categorias} 
+            onChange={handSelectCategoria}
+            defaultValue={{label: "Escoger", value: "Escoger"}}
+            
+            >
+            
+            </Select >
+            <hr></hr>
+            <h4 className="subtitulo">Valor</h4>
+            <Select 
+            className="opciones" 
+            options={valores} 
+            onChange={handSelectValor}
+            defaultValue={{label: "Escoger", value: "Escoger"}}
+            
+            >
+                
+            </Select >
+            <hr></hr>
+            <h4 className="subtitulo">Instituciones</h4>
+           
+            <Select 
+            className="opciones" 
+            options={instituciones} 
+            onChange={handSelectInstitucion}
+            defaultValue={{label: "Escoger", value: "Escoger"}}
+            
+            >
+            
+            </Select >
+    </div>
+    )
+}  
+export default Filtro;
