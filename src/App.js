2e2e707e958b45aad8b772b48cbae4863c0c811a
Nicole Solano Gómez <nicole.nasg@gmail.com>
2023-11-02 00:23:07 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import VerticalCards from './components/Tarjeta-pagina-terciaria/Tarjeta-pagina-terciaria';
 
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div>
       <h1>Tarjetas</h1>
       <VerticalCards />
+    
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { getTopNav } from "./components/Navbar/data/navbars";
+
+const Navbar = () => {
+  const [navItems, setNavItems] = useState([]);
+  const [collapse, setCollapse] = useState("nav__menu");
+  const [toggleIcon, setToggleIcon] = useState("toggler__icon");
+
+  useEffect(() => {
+    setNavItems(getTopNav());
+  }, []);
+
+  const onToggle = () => {
+    collapse === "nav__menu"
+      ? setCollapse("nav__menu nav__collapse")
+      : setCollapse("nav__menu");
+
+    toggleIcon === "toggler__icon"
+      ? setToggleIcon("toggler__icon toggle")
+      : setToggleIcon("toggler__icon");
+  };
+
+  return (
+    <div className="nav__wrapper">
+      <div className="container">
+        <nav className="nav">
+          <a href="#" className="nav__brand">
+          <img src="logo.png"  />
+          </a>
+          <ul className={collapse}>
+            {navItems.map((item) => (
+              <li key={item.id} className="nav__item">
+                <a href={item.href} className="nav__link">
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className={toggleIcon} onClick={onToggle}>
+            <div className="line__1"></div>
+            <div className="line__2"></div>
+            <div className="line__3"></div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
