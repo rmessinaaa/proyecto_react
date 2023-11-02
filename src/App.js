59d03355b91c0b0833d151efdf0d60e7678c32d4
Nicole Solano Gómez <nicole.nasg@gmail.com>
2023-11-02 00:39:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import VerticalCards from './components/Tarjeta-pagina-terciaria/Tarjeta-pagina-terciaria';
 
-import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div>
       <h1>Tarjetas</h1>
       <VerticalCards />
     
-      <Footer/>
     </div>
   );
 }
