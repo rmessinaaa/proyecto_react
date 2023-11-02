8d3b7d21bbc64f75c08095d2524f4acdcbc6ddac
JersonAmir <147181633+JersonAmir@users.noreply.github.com>
2023-11-02 15:34:06 -0300

    Componentes Insignias.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import VerticalCards from './components/Tarjeta-pagina-terciaria/Tarjeta-pagina-terciaria';
+
 
 function App() {
   return (
     <div>
-      <h1>Tarjetas</h1>
-      <VerticalCards />
+
+  
     
     </div>
   );
