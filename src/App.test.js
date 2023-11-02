7e81084c8770f295fc7fd1064414ff4799b67b92
Renato Messina <138999302+rmessinaaa@users.noreply.github.com>
2023-11-02 16:53:25 -0300

    listo

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App.js';
 
-import logo from './logo.svg';
-import './App.css';
-// borrar modulos importados
-
-
-function App() {
-  return (
-    <div className="App">
-      {/* Pruebas: Prueba lo que quieras, pero esto tiene que quedar vacío, lo que insertes para ver el renderizado, después tienes que eliminarlo */}
-    </div>
-  );
-}
-
-export default App;
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
