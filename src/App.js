4f5544d202821c180dd4eba4444f9e8cf2f4602b
Renato Messina <138999302+rmessinaaa@users.noreply.github.com>
2023-11-02 15:15:02 -0300

    listo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-<<<<<<< HEAD
-=======
+
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 // eslint-disable-next-line
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PaginaInicio from './components/pagina-inicio/pagina-inicio';
 import PaginaNosotros from './components/pagina-nosotros/pagina-nosotros';
 import Navbar from './components/Navbar/Navbar';
 import Perfil from './components/Perfil/Perfil'
-
-
-
-=======
->>>>>>> 09dc7edbb6d469537a81bc9459b4343f44277012
->>>>>>> aa07e55185e5ca54558ff69307a300c1a81d836e
-
 import React from "react";
 import TarjetaG from "./components/Tarjeta-grande/Tarjeta-grande";
 function App() {
-<<<<<<< HEAD
     return (
         <div className="App">
-            <h1>Mi Aplicación React</h1>
-            <TarjetaG /> {/* Muestra la tarjeta en la aplicación */}
+          
         </div>
     );
-=======
   return (
     <div>
       <h1>Tarjetas</h1>
@@ -34,7 +22,6 @@ function App() {
     
     </div>
   );
->>>>>>> 09dc7edbb6d469537a81bc9459b4343f44277012
 }
 
 export default App;
