887f7d068f77abe586db0391511bc77a6c1b185b
Renato Messina <138999302+rmessinaaa@users.noreply.github.com>
2023-11-02 15:31:21 -0300

    listo

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PaginaInicio from './components/pagina-inicio/pagina-inicio';
 import PaginaNosotros from './components/pagina-nosotros/pagina-nosotros';
+import PaginaBuscador from './components/pagina-buscador/PaginaBuscador'
+import PaginaDonaciones from './components/pagina-donaciones/pagina-donaciones'; 
+import PaginaPerfil from './components/pagina-perfil/pagina-perfil'; 
 import Navbar from './components/Navbar/Navbar';
 import Perfil from './components/Perfil/Perfil'
 import React from "react";
 import TarjetaG from "./components/Tarjeta-grande/Tarjeta-grande";
 function App() {
     return (
-        <div className="App">
-          
+        <div>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<PaginaInicio/>}></Route>
+              <Route path='/nosotros' element={<PaginaNosotros/>}></Route>
+              <Route path='/buscador' element={<PaginaBuscador/>}></Route>
+              <Route path='/donaciones' element={<PaginaDonaciones/>}></Route>
+              <Route path='/perfil' element={<PaginaPerfil/>}></Route>
+            </Routes>
+          </BrowserRouter>
         </div>
     );
   return (
