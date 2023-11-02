05cb913daee6f8f64b9e7878d8f15356a9ce9522
Renato Messina <138999302+rmessinaaa@users.noreply.github.com>
2023-11-02 11:59:36 -0300

    listo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import VerticalCards from './components/Tarjeta-pagina-terciaria/Tarjeta-pagina-terciaria';
 
 function App() {
   return (
     <div>
-      <h1>Tarjetas</h1>
-      <VerticalCards />
+      
     
     </div>
   );
