6464efff1fce6988ef5f7540488bbbc39d01802e
JersonAmir <147181633+JersonAmir@users.noreply.github.com>
2023-11-02 17:39:00 -0300

    Listo

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,16 @@
 import { render, screen } from '@testing-library/react';
-<<<<<<< HEAD
+
 import App from './App';
-=======
+
 import App from './App.js';
 
->>>>>>> 66529fce7df149422797d8714996b53de1b409de
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> 66529fce7df149422797d8714996b53de1b409de
+
+
+
