ceeb92e2d72e5acaa54f5016e102c5477eace3b9
Renato Messina <138999302+rmessinaaa@users.noreply.github.com>
2023-11-02 15:06:05 -0300

    añadiendo funciones a donaciones y perfil (paginas)

---
+++ src/components/pagina-donaciones/pagina-donaciones.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PaginaDonaciones(){
+    return(
+        <h1>Donaciones</h1>
+    )
+}
---
+++ src/components/pagina-perfil/pagina-perfil.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PaginaPerfil(){
+    return(
+        <h1>Pagina Perfil</h1>
+    )
+}
