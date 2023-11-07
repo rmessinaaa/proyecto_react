0efa99eaef7fbc9a35ce135b9ce33f72aa5869ab
Nicole Solano Gómez <nicole.nasg@gmail.com>
2023-11-07 10:49:24 -0300

    Slider Inicio

--- src/components/pagina-inicio/pagina-inicio.jsx
+++ src/components/pagina-inicio/pagina-inicio.jsx
@@ -4,15 +4,17 @@ import SubsHomeTusDonaciones from '../subs-home-tus-donaciones/subs-home-tus-don
 import SubsHomeNosotros from '../subs-home-nosotros/subs-home-nosotros';
 import SubsHomePerfil from '../subs-home-perfil/subs-home-perfil';
 import SubsHomeCampañas from '../subs-home-campañas/subs-home-campañas';
-
+import Carrusel from "../Carrusel/Carrusel"; 
 function PaginaInicio() {
     return (
         <div className='bg-paginas'>
+            <Carrusel/>
             <div className="container-fluid">
                 <div className='container container-blanco'>
                     <h1 className='titulos-principales'>Inicio</h1>
                 </div>
             </div>
+    
             <Buscador />
             <SubsHomeCampañas />
             <SubsHomeTusDonaciones />
