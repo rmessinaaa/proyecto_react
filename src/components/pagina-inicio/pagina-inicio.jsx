68192560b73a8e274c5622a1979b97d11c0981f8
Nicole Solano Gómez <nicole.nasg@gmail.com>
2023-11-08 09:57:25 -0300

    inicio de sesion
    Esta al final del inicio para ver

--- src/components/pagina-inicio/pagina-inicio.jsx
+++ src/components/pagina-inicio/pagina-inicio.jsx
@@ -5,21 +5,24 @@ import SubsHomeNosotros from '../subs-home-nosotros/subs-home-nosotros';
 import SubsHomePerfil from '../subs-home-perfil/subs-home-perfil';
 import SubsHomeCampañas from '../subs-home-campañas/subs-home-campañas';
 import Carrusel from "../Carrusel/Carrusel"; 
+import Iniciosesion from "../Inicio-sesion/Inicio-sesion"
 function PaginaInicio() {
     return (
         <div className='bg-paginas'>
             <Carrusel/>
+            
             <div className="container-fluid">
                 <div className='container container-blanco'>
                     <h1 className='titulos-principales'>Inicio</h1>
                 </div>
             </div>
-    
+            
             <Buscador />
             <SubsHomeCampañas />
             <SubsHomeTusDonaciones />
             <SubsHomeNosotros />
             <SubsHomePerfil />
+            <Iniciosesion />
         </div>
     )
 }
