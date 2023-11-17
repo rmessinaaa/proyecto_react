176cdda51b751708f0362654a01f0b2be7dec71c
JersonAmir <147181633+JersonAmir@users.noreply.github.com>
2023-11-17 13:00:45 -0300

    ojito implementado

--- src/components/Iniciar-sesión/Iniciar-sesión.jsx
+++ src/components/Iniciar-sesión/Iniciar-sesión.jsx
@@ -11,6 +11,7 @@ const IniciarSesion = () => {
   const [password, setPassword] = useState('');
   const [userData, setUserData] = useState({});
   const [showPopup, setShowPopup] = useState(false);
+  const [mostrarPassword, setMostrarPassword] = useState(false);
   
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -130,6 +131,10 @@ const IniciarSesion = () => {
     setShowPopup(!showPopup);
   };
 
+  const toggleMostrarPassword = () => {
+    setMostrarPassword(!mostrarPassword);
+  };
+
   return (
     <div className='row'>
       <div className='col-1'></div>
@@ -167,16 +172,26 @@ const IniciarSesion = () => {
           </form>
         </div>
         <div className='container-fluid div-tres'>
-          <form>
-            <input
-              className='input'
-              type='password'
-              placeholder='Contraseña'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </form>
-        </div>
+        <form>
+          <input
+            className='input'
+            type={mostrarPassword ? 'text' : 'password'}
+            placeholder='Contraseña'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Link className='mostrar' type='button' onClick={toggleMostrarPassword}>
+            {mostrarPassword ? <svg className='mostrar' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+</svg> : <svg className='mostrar' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+</svg>}
+            
+          </Link>
+        </form>
+      </div>
         <div className='container-fluid div-cuatro'>
           <button onClick={handleSubmit} className='dos-botones'>Siguiente</button>
         </div>
