2365e4c51ce466d9c09920702a50c4f7dc7d06b3
Renato Messina <138999302+rmessinaaa@users.noreply.github.com>
2023-11-20 11:58:30 -0300

    campaign crar

--- src/components/subs-buscador-resultados/subs-buscador-resultados.jsx
+++ src/components/subs-buscador-resultados/subs-buscador-resultados.jsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import VerticalCards from '../Tarjeta-pagina-terciaria/Tarjeta-pagina-terciaria';
 import './subs-buscador-resultados.css'
 
+
 function SubsBuscadorResultados () {
+    const [data, setData] = useState([]);
+
+    const [nombres, setNombres] = useState([]);
+
+    const token = 'rxtcfygvbhjkklmñl'
+
+  useEffect(() => {
+    setNombres(data);
+  }, [data])
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/allcampanas", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        credentials : 'include'
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => setData(data))
+    .catch((error) => {
+      console.error('Error en la solicitud:', error);
+    });
+  }, []);
+  
+    
+    
+    
     return (
         <div>
             <div className="row align-items-center justify-content-center centrar contenido">
                 <div className="col-md-4 text-center centrar-contenido">
-                    <VerticalCards />
+                    {nombres.map((campaign) => (<VerticalCards
+                      key={campaign._id}
+                      title={campaign.title}
+                      description={campaign.description}
+            
+                    /> ))} 
                 </div>
-                <div className="col-md-4 text-center centrar contenido">
-                    <VerticalCards />
-                </div>
-                <div className="col-md-4 text-center centrar contenido">
-                    <VerticalCards />
-                </div>
+                
             </div>
             
         </div>
