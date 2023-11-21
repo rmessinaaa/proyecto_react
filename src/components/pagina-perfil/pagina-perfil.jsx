2ab4fe138449fddc9de696abf957902ecca08ec0
JersonAmir <147181633+JersonAmir@users.noreply.github.com>
2023-11-21 20:29:00 -0300

    Perfil con datos

--- src/components/pagina-perfil/pagina-perfil.jsx
+++ src/components/pagina-perfil/pagina-perfil.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
-import Infopersonal from '../informacionPersonal/infoPersonal';
+import EditarPerfil from '../informacionPersonal/infoPersonal'
 import Perfil from '../Perfil/Perfil';
 import "./paginaperfil.css";
+import {useState, useEffect} from "react";
 // import SubsPerfilIntereses from '../subs-perfil-intereses/subs-perfil-intereses';
 
 function PaginaPerfil() {
+  const [data, setData] = useState([]);
+  const [usuario, setUsuario] = useState([]);
+  // const [username, setUsername] = useState([]);
+  // const [email, setEmail] = useState([]);
+  // const [password, setPassword] = useState([]);
+  const token = window.localStorage.getItem("token")
+  const username = window.localStorage.getItem("username")
+  const email = window.localStorage.getItem("email")
+
+
+ 
+
+  useEffect(() => {
+    setUsuario(data);
+    // setUsername(data.username);
+    // setPassword(data.password);
+    // setEmail(data.email);
+  }, [data])
+  
+  useEffect(() => {
+    const id = window.localStorage.getItem("id")
+    const idarreglado = id.replace(/['"]+/g, '')
+
+    fetch(`http://localhost:8080/api/profile/${idarreglado}`, {
+      method: 'GET',
+      headers: { 
+        'Content-Type': 'application/json',
+        credentials : 'include',
+        acces_token: token
+      }
+    })
+    .then((res) => res.json())
+    .then((data) => setData(data))
+    .catch((error) => {
+      console.error('Error en la solicitud:', error);
+    });
+  }, []);
+
+
+console.log(data)
+
   return (
     <div className='bg-paginas bg-perfil'>
       <div className="container-fluid">
@@ -17,12 +59,23 @@ function PaginaPerfil() {
         </div>
       </div>
       <div className="row">
-        <div className="col ">">
+        <div className="col ">
           <Perfil />
         </div>
         <div className="col >">
           <div className='centrar-contenido'>
-            <Infopersonal />
+            <EditarPerfil 
+            username = {username}
+            email = {email} 
+            />
+          {/* {usuario.map((userProfile) => (<EditarPerfil
+                      key={userProfile._id}
+                      username={userProfile.username}
+                      email={userProfile.email}
+                      password={userProfile.password}
+            
+                    /> ))}  */}
+           
           </div>
         </div>
       </div>
